7d1da40b88f97079a4424c78254c7115c386f483
Ayeen Poostforoushan <apoostforoushan@hotmail.com>
2023-05-07 00:39:53 +0330

    Placeholder for unit target constants

--- src/main/java/Settings/UnitConstants/AttackingUnitConstants.java
+++ src/main/java/Settings/UnitConstants/AttackingUnitConstants.java
@@ -1,5 +1,6 @@
 package Settings.UnitConstants;
 
+import model.enums.AttackTarget;
 import model.enums.MapAssetType;
 import model.enums.Weapon;
 
@@ -9,32 +10,74 @@ import java.util.Collection;
 import java.util.List;
 
 public enum AttackingUnitConstants {
-    DOG(1, 8, 0, 70, 80, 1, false, false, MapAssetType.DOG, 100, null),
+    DOG(1, 8, 0, 70, 80, 1, false
+            , false, MapAssetType.DOG, 100, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
     //European Units :
-    ARCHER(1, 8, 0, 500, 20, 3, true, true, MapAssetType.ARCHER, 30, new ArrayList<>(List.of(Weapon.BOW))),
-    CROSSBOWMAN(0.8, 3, 0, 800, 50, 2, false, false, MapAssetType.CROSSBOWMAN, 60, new ArrayList<>(List.of(Weapon.BOW, Weapon.LEATHER_ARMOUR))),
-    SPEARMAN(1, 5, 0, 600, 100, 1, true, true, MapAssetType.SPEARMAN, 30, new ArrayList<>(List.of(Weapon.SPEAR))),
-    PIKEMAN(0.6, 3, 0, 1700, 250, 1, false, true, MapAssetType.PIKEMAN, 60, new ArrayList<>(List.of(Weapon.PIKE))),
-    MACEMAN(0.8, 6, 0, 1500, 175, 1, true, true, MapAssetType.MACEMAN, 90, new ArrayList<>(List.of(Weapon.MACE, Weapon.METAL_ARMOUR))),
-    SWORDSMAN(0.5, 1, 0, 2000, 350, 1, false, false, MapAssetType.SWORDSMAN, 100, new ArrayList<>(List.of(Weapon.SWORD, Weapon.METAL_ARMOUR))),
-    KNIGHT(0.5, 10, 0, 2000, 300, 1, false, false, MapAssetType.KNIGHT, 120, new ArrayList<>(List.of(Weapon.SWORD, Weapon.METAL_ARMOUR, Weapon.HORSE))),
+    ARCHER(1, 8, 0, 500, 20, 3, true,
+            true, MapAssetType.ARCHER, 30, new ArrayList<>(List.of(Weapon.BOW)),
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    CROSSBOWMAN(0.8, 3, 0, 800, 50, 2, false,
+            false, MapAssetType.CROSSBOWMAN, 60, new ArrayList<>(List.of(Weapon.BOW, Weapon.LEATHER_ARMOUR)),
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    SPEARMAN(1, 5, 0, 600, 100, 1, true,
+            true, MapAssetType.SPEARMAN, 30, new ArrayList<>(List.of(Weapon.SPEAR)),
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    PIKEMAN(0.6, 3, 0, 1700, 250, 1, false,
+            true, MapAssetType.PIKEMAN, 60, new ArrayList<>(List.of(Weapon.PIKE)),
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    MACEMAN(0.8, 6, 0, 1500, 175, 1, true,
+            true, MapAssetType.MACEMAN, 90, new ArrayList<>(List.of(Weapon.MACE, Weapon.METAL_ARMOUR)),
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    SWORDSMAN(0.5, 1, 0, 2000, 350, 1, false,
+            false, MapAssetType.SWORDSMAN, 100, new ArrayList<>(List.of(Weapon.SWORD, Weapon.METAL_ARMOUR)),
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    KNIGHT(0.5, 10, 0, 2000, 300, 1, false,
+            false, MapAssetType.KNIGHT, 120, new ArrayList<>(List.of(Weapon.SWORD, Weapon.METAL_ARMOUR, Weapon.HORSE)),
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
     //TODO fill this
-    TUNNELER(0, 0, 0, 0, 0, 0, false, false, MapAssetType.TUNNELER, 10, null),
-    BLACK_MONK(1, 3, 0, 500, 50, 1, false, false, MapAssetType.BLACK_MONK, 15, null),
+    TUNNELER(0, 0, 0, 0, 0, 0, false,
+            false, MapAssetType.TUNNELER, 10, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    BLACK_MONK(1, 3, 0, 500, 50, 1, false,
+            false, MapAssetType.BLACK_MONK, 15, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
     //Arabian Units:
-    ARCHER_BOW(1, 8, 0, 500, 20, 3, false, true, MapAssetType.ARCHER_BOW, 60, null),
-    SLAVE(1, 8, 0, 100, 20, 1, false, true, MapAssetType.SLAVE, 5, null),
+    ARCHER_BOW(1, 8, 0, 500, 20, 3, false,
+            true, MapAssetType.ARCHER_BOW, 60, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    SLAVE(1, 8, 0, 100, 20, 1, false,
+            true, MapAssetType.SLAVE, 5, null,
+            new ArrayList<>(List.of(AttackTarget.HOUSE))),
 
-    SLINGER(1, 8, 0, 150, 20, 2, false, false, MapAssetType.SLINGER, 10, null),
-    ASSASSIN(1, 5, 0, 1000, 100, 1, false, false, MapAssetType.ASSASSIN, 60, null),
-    HORSE_ARCHER(1, 10, 0, 1000, 50, 3, false, false, MapAssetType.HORSE_ARCHER, 80, null),
-    ARABIAN_SWORDSMAN(0.6, 5, 0, 1500, 275, 1, false, false, MapAssetType.ARABIAN_SWORDSMAN, 85, null),
-    FIRE_THROWER(1, 8, 0, 800, 100, 2, false, false, MapAssetType.FIRE_THROWER, 90, null),
+    SLINGER(1, 8, 0, 150, 20, 2, false,
+            false, MapAssetType.SLINGER, 10, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    ASSASSIN(1, 5, 0, 1000, 100, 1, false,
+            false, MapAssetType.ASSASSIN, 60, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    HORSE_ARCHER(1, 10, 0, 1000, 50, 3, false,
+            false, MapAssetType.HORSE_ARCHER, 80, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    ARABIAN_SWORDSMAN(0.6, 5, 0, 1500, 275, 1, false,
+            false, MapAssetType.ARABIAN_SWORDSMAN, 85, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    FIRE_THROWER(1, 8, 0, 800, 100, 2, false,
+            false, MapAssetType.FIRE_THROWER, 90, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS, AttackTarget.HOUSE))),
     //Siege warfare:
-    BATTERING_RAM(1, 3, 4, 400, 500, 1, false, false, MapAssetType.BATTERING_RAM, 20, null),
-    CATAPULT(1, 3, 2, 400, 400, 3, false, false, MapAssetType.CATAPULT, 40, null),
-    STABLE_CATAPULT(1, 0, 3, 500, 500, 4, false, false, MapAssetType.STABLE_CATAPULT, 50, null),
-    FIRE_CATAPULT(1, 3, 2, 400, 300, 2, false, false, MapAssetType.FIRE_CATAPULT, 100, null);
+    BATTERING_RAM(1, 3, 4, 400, 500, 1, false,
+            false, MapAssetType.BATTERING_RAM, 20, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS))),
+    CATAPULT(1, 3, 2, 400, 400, 3, false,
+            false, MapAssetType.CATAPULT, 40, null,
+            new ArrayList<>(List.of(AttackTarget.BUILDING))),
+    STABLE_CATAPULT(1, 0, 3, 500, 500, 4, false,
+            false, MapAssetType.STABLE_CATAPULT, 50, null,
+            new ArrayList<>(List.of(AttackTarget.BUILDING))),
+    FIRE_CATAPULT(1, 3, 2, 400, 300, 2, false,
+            false, MapAssetType.FIRE_CATAPULT, 100, null,
+            new ArrayList<>(List.of(AttackTarget.TROOPS)));
     private final double defenseMultiplier;
     private final int moveSpeed;
     private final int engineerCount;
@@ -46,10 +89,11 @@ public enum AttackingUnitConstants {
     private final MapAssetType type;
     private final int cost;
     private final ArrayList<Weapon> weapon;
+    private final ArrayList<AttackTarget> targets;
 
     AttackingUnitConstants(double defenseMultiplier, int moveSpeed, int engineerCount, double maxHitPoint, int attackDamage
             , int attackRange, boolean canClimbLadder, boolean canModifyMoat, MapAssetType type,
-                           int cost, ArrayList<Weapon> weapon) {
+                           int cost, ArrayList<Weapon> weapon, ArrayList<AttackTarget> targets) {
         this.defenseMultiplier = defenseMultiplier;
         this.moveSpeed = moveSpeed;
         this.engineerCount = engineerCount;
@@ -61,6 +105,7 @@ public enum AttackingUnitConstants {
         this.type = type;
         this.cost = cost;
         this.weapon = weapon;
+        this.targets = targets;
     }
 
     public double getDefenseMultiplier() {
--- src/main/java/model/enums/AttackTarget.java
+++ src/main/java/model/enums/AttackTarget.java
@@ -1,5 +1,21 @@
 package model.enums;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static model.enums.MapAssetType.*;
+
 public enum AttackTarget {
-    WALL, HOUSE, TROOPS, HIGH_TROOPS
+    //TODO fill these
+    BUILDING(new ArrayList<>(List.of(ARCHER, CROSSBOWMAN, SPEARMAN, PIKEMAN, MACEMAN, SWORDSMAN, KNIGHT, TUNNELER, BLACK_MONK, ARCHER_BOW, SLAVE, SLINGER, ASSASSIN,
+            HORSE_ARCHER, ARABIAN_SWORDSMAN, FIRE_THROWER, ENGINEER, LADDER_MAN, DOG, COW))),
+    HOUSE(new ArrayList<>(List.of(HEADQUARTER,BEAR_FACTORY, BAKERY, WHEAT_FIELD,
+            GRAIN_FIELD, DIARY_FACTORY, APPLE_GARDEN,POLTURNER, FLETCHER, BLACKSMITH, ARMOURER, WOOD_CUTTER, QUARRY))),
+    TROOPS(new ArrayList<>(List.of(ARCHER, CROSSBOWMAN, SPEARMAN, PIKEMAN, MACEMAN, SWORDSMAN, KNIGHT, TUNNELER, BLACK_MONK, ARCHER_BOW, SLAVE, SLINGER, ASSASSIN,
+            HORSE_ARCHER, ARABIAN_SWORDSMAN, FIRE_THROWER, ENGINEER, LADDER_MAN, DOG, COW)));
+    private final ArrayList<MapAssetType> targets;
+
+    AttackTarget(ArrayList<MapAssetType> targets) {
+        this.targets = targets;
+    }
 }
