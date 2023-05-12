8eaf1ffd809f4484cd7df42313e811bd44ebf10b
kianGs4 <kian.ghassemi1383@gmail.com>
2023-05-12 17:33:57 +0330

    added update highScore after game

--- src/main/java/utils/Vector2D.java
+++ src/main/java/utils/Vector2D.java
@@ -13,13 +13,6 @@ public class Vector2D {
         return Math.abs(v2.x - x) + Math.abs(v2.y - y);
     }
 
-    public static int translateVector2DToInt(Vector2D v2, int size) {
-        return v2.x * size + v2.y;
-    }
-
-    public static Vector2D translateIntToVector2D(int location, int size) {
-        return new Vector2D(location / size, location % size);
-    }
 
     @Override
     public boolean equals(Object obj) {
