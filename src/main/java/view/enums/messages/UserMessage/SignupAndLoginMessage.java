41ec5af362388405527e7e98a33e1a1c2642b7a2
kianGs4 <kian.ghassemi1383@gmail.com>
2023-05-23 13:18:23 +0330

    fix minor bugs

--- src/main/java/view/enums/messages/UserMessage/SignupAndLoginMessage.java
+++ src/main/java/view/enums/messages/UserMessage/SignupAndLoginMessage.java
@@ -8,13 +8,13 @@ public enum SignupAndLoginMessage {
     PASSWORD_WEEK_LETTERS_PROBLEM("Password is week(password must include at least 1 lowercase character" +
             ", 1 uppercase character, 1 number, and 1 special character)"),
     EXISTING_USERNAME("The username already taken"),
-    CONFIRMATION_ERROR("Passwords do not match"),
+        CONFIRMATION_ERROR("Passwords do not match"),
     EXISTED_EMAIL("The email already taken"),
     RANDOM_PASSWORD,
     PASSWORD_CHANGE_SUCCESSFULLY("Password changed successfully"),
     PICKING_QUESTION("Pick your security question: 1. What is my father’s name?" +
             " 2. What was my first pet’s name? 3. What is my mother’s last name?"),
-    FAIL_PICKING_UP_QUESTION("some error founds"),
+    FAIL_PICKING_UP_QUESTION("Some error found"),
     SUCCESS_CREATING_USER("user created successfully"),
     USER_DOES_NOT_EXIST("There is no user with this username"),
     INCORRECT_PASSWORD("The password entered is incorrect"),
