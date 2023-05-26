ce58db107fbcb752fecbfed25e19f74e76c2b080
kianGs4 <kian.ghassemi1383@gmail.com>
2023-05-26 19:12:23 +0330

    added css file

--- src/main/java/view/UserMenus/LoginMenu.java
+++ src/main/java/view/UserMenus/LoginMenu.java
@@ -1,6 +1,8 @@
 package view.UserMenus;
 
 import controller.UserControllers.LoginController;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import utils.FormatValidation;
 import utils.SignupAndLoginUtils;
 import view.Menu;
@@ -11,7 +13,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
-public class LoginMenu {
+public class LoginMenu extends Application {
     private final LoginController loginController;
     private final Scanner scanner;
 
@@ -20,77 +22,9 @@ public class LoginMenu {
         this.scanner = Menu.getScanner();
     }
 
-    public String run() {
-        String nextCommand;
-        Matcher matcher;
-        while (true) {
-            nextCommand = scanner.nextLine();
-            SignupAndLoginCommand typeOfCommand = SignupAndLoginCommand.getCommand(nextCommand);
-            if (typeOfCommand == null) {
-                SignupAndLoginMessage.INVALID_COMMAND.printMessage();
-                continue;
-            }
-            matcher = SignupAndLoginCommand.getMatcher(nextCommand, typeOfCommand);
-            switch (typeOfCommand) {
-                case EXIT:
-                    return "exit";
-                case LOGIN:
-                    if (loginCall(matcher)) return "login";
-                case CHANGE_PASSWORD:
-                    changePasswordCall(matcher);
-                    break;
-                case SIGNUP_MENU:
-                    SignupAndLoginMessage.SIGNUP_MENU.printMessage();
-                    return "signup menu";
-            }
-        }
-    }
 
-    private boolean loginCall(Matcher matcher) {
-        HashMap<String, String> inputs = SignupAndLoginUtils.getInputs(matcher, SignupAndLoginCommand.LOGIN.getRegex());
-        SignupAndLoginMessage message = loginController.login(inputs);
-        message.printMessage();
-        if (message.equals(SignupAndLoginMessage.TOO_MANY_ATTEMPTS)) {
-            tooManyAttemptsError();
-            return false;
-        }
-        if (message.equals(SignupAndLoginMessage.SUCCESS_PROCESS)) {
-            System.out.println("success");
-            return true;
-        }
-        return false;
-    }
+    @Override
+    public void start(Stage stage) throws Exception {
 
-    private void changePasswordCall(Matcher matcher) {
-        SignupAndLoginMessage message = loginController.checkUserExist(matcher.group("username"));
-        message.printMessage();
-        String nextCommand = Menu.getScanner().nextLine();
-        System.out.println(loginController.currentUser.getPasswordRecoveryQuestion());
-        if (loginController.currentUser.isRecoveryPasswordCorrect(nextCommand)) {
-            recoveryPassword();
-        } else System.out.println("Incorrect answer!");
-    }
-
-    private void tooManyAttemptsError() {
-        int minutes = (int) (loginController.getTimeUntilLogin() / 60);
-        int seconds = (int) (loginController.getTimeUntilLogin() % 60);
-        System.out.println("Too many failed attempts. Please wait " + minutes + " minutes and " +
-                seconds + " seconds before trying again");
-    }
-
-    private void recoveryPassword() {
-        String nextCommand;
-        while (true) {
-            System.out.println("Enter new Password:");
-            nextCommand = Menu.getScanner().nextLine();
-            if (!FormatValidation.isFormatValid(nextCommand, FormatValidation.PASSWORD_LENGTH)) {
-                SignupAndLoginMessage.PASSWORD_WEEK_LENGTH.printMessage();
-            } else if (!FormatValidation.isFormatValid(nextCommand, FormatValidation.PASSWORD_LETTERS))
-                SignupAndLoginMessage.PASSWORD_WEEK_LETTERS_PROBLEM.printMessage();
-            else {
-                loginController.currentUser.setPassword(nextCommand);
-                break;
-            }
-        }
     }
 }
