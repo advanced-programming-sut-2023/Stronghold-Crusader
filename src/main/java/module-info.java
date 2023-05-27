db4f475158bd65d8426bc67bf6a32f316fd2a6bf
Ayeen Poostforoushan <43296075+ayeenp@users.noreply.github.com>
2023-05-27 09:57:48 +0330

    Fixed module problems

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,11 @@ module project.group {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires com.google.gson;
+    requires java.desktop;
 
+    exports model;
     exports view;
+    opens model to com.google.gson;
     opens view to javafx.fxml;
 }
