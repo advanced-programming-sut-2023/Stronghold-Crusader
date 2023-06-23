7dd656591cdbedfb6a365c7e60f3d8c8e4f0d965
kianGs4 <kian.ghassemi1383@gmail.com>
2023-06-23 15:09:10 +0330

    added trade menu for new trade situation

--- src/main/java/view/GameMenus/TradeMenu.java
+++ src/main/java/view/GameMenus/TradeMenu.java
@@ -4,24 +4,38 @@ import controller.GameControllers.TradeController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.User.Player;
+import model.enums.AssetType.Material;
 import utils.SignupAndLoginUtils;
 import view.Menu;
 import view.enums.commands.TradeMenuCommand;
 import view.enums.messages.TradeMenuMessage;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 
 public class TradeMenu extends Application {
+    public Button donateButton;
+    public Button requestButton;
+    public Text number;
+    public Circle acceptedMaterial;
+    public Button confirmButton;
     TradeController tradeController;
+    private final HashMap<Circle, Material> materials = new HashMap<>();
+
     Stage stage = new Stage();
+
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
@@ -104,11 +118,49 @@ public class TradeMenu extends Application {
             imageView.setFitWidth(50);
             imageView.setImage(new Image(player.getAvatarPath()));
             hBox.getChildren().add(imageView);
-            imageView.setOnMouseClicked(mouseEvent -> {createNewTrade(player);});
+            imageView.setOnMouseClicked(mouseEvent -> {
+                try {
+                    createNewTrade(player);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
     }
 
-    private void createNewTrade(Player player) {
+    private void createNewTrade(Player player) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(TradeMenu.class.getResource("/FXML/Gamefxml/TradeMenus/tradeMenu.fxml"));
+        donateButton.setDisable(true);
+        requestButton.setDisable(true);
+        confirmButton.setDisable(true);
+        int counter = 0;
+        for (Material material : Material.values()) {
+            Circle circle = new Circle(23);
+            circle.setFill(new ImagePattern(material.getImage()));
+            circle.setTranslateX(87 + 70 * counter);
+            circle.setTranslateY(63 + (int) (counter / 9) * 65);
+            circle.setOnMouseClicked(mouseEvent -> {
+                acceptedMaterial = circle;
+                donateButton.setDisable(false);
+                requestButton.setDisable(false);
+            });
+            materials.put(circle, material);
+            counter++;
+        }
+        anchorPane.getChildren().addAll(materials.keySet());
+        stage.setScene(new Scene(anchorPane));
+
+    }
+
+    public void plus(MouseEvent mouseEvent) {
+        int num = Integer.parseInt(number.getText());
+        number.setText(Integer.valueOf(num + 1).toString());
+    }
+
+    public void minus(MouseEvent mouseEvent) {
+        int num = Integer.parseInt(number.getText());
+        if (num == 0) num += 1;
+        number.setText(Integer.valueOf(num - 1).toString());
 
     }
 }
