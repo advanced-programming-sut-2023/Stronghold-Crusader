55c62784c3c8178df9db4ed51bdec20eef8dd73d
kianGs4 <kian.ghassemi1383@gmail.com>
2023-06-29 14:24:19 +0330

    added button ands css for friendsTable

--- client/src/main/java/view/UserMenus/FriendsTable.java
+++ client/src/main/java/view/UserMenus/FriendsTable.java
@@ -3,6 +3,7 @@ package view.UserMenus;
 import controller.UserControllers.FriendsMenuController;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import model.User.User;
@@ -15,10 +16,16 @@ public class FriendsTable {
     private final Circle avatar = new Circle();
     private final String username;
     private final Button accept = new Button("select");
+    private final ImageView isFriend = new ImageView();
+    private final String email;
 
     public FriendsTable(User user) {
+        email = user.getEmail();
+        setIsFriendImage(FriendsMenuController.getCurrentUser().isFriend(user));
         avatar.setFill(new ImagePattern(new Image(user.getAvatarPath())));
         avatar.setRadius(24);
+        isFriend.setFitHeight(35);
+        isFriend.setFitWidth(35);
         this.username = user.getUsername();
         accept.setOnMouseClicked(e -> {
             try {
@@ -28,6 +35,12 @@ public class FriendsTable {
                 throw new RuntimeException(ex);
             }
         });
+    }
+
+    private void setIsFriendImage(boolean isFriend) {
+        if (isFriend)
+            this.isFriend.setImage(new Image(FriendsTable.class.getResource("/assets/icons/accepted.png").toString()));
+        else this.isFriend.setImage(new Image(FriendsTable.class.getResource("/assets/icons/deny.png").toString()));
     }
 
 
@@ -41,5 +54,13 @@ public class FriendsTable {
 
     public Button getAccept() {
         return accept;
+    }
+
+    public ImageView getIsFriend() {
+        return isFriend;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
