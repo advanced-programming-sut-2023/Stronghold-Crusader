7a7c6493bc0ac12618459fd94e679e722e0d7b32
Ayeen Poostforoushan <43296075+ayeenp@users.noreply.github.com>
2023-06-30 18:05:32 +0330

    Fixed small bugs

--- client/src/main/java/Settings/Settings.java
+++ client/src/main/java/Settings/Settings.java
@@ -7,5 +7,6 @@ public class Settings {
     public static final String CONFIGS_PATH = RESOURCE_PATH + "config/";
     public static final String GAME_CONSTANTS_PATH = CONFIGS_PATH + "constants.cfg";
     public static final String MARKET_CONSTANTS_PATH = CONFIGS_PATH + "prices.json";
+    public static final String DEFAULT_AVATAR = "/assets/avatars/defaults/default.png";
 
 }
